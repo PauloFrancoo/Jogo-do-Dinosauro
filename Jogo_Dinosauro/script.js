5ff97cab2c045e5f92e000ad77f167fd2a47ca1b
PauloFrancoo <43294609+PauloFrancoo@users.noreply.github.com>
2022-06-24 11:18:14 -0300

    Add files via upload

---
+++ Jogo_Dinosauro/script.js
@@ -0,0 +1,72 @@
+const dino = document.querySelector('.dino');
+const background = document.querySelector('.container');
+
+let isJumping = false;// se está pulando
+let isGameOver = false; // se fim de jogo
+let position = 0;
+
+function handleKeyUp(event) {
+  if (event.keyCode === 32) { //espaço
+    if (!isJumping) {
+      jump();
+    }
+  }
+}
+
+function jump() {
+  isJumping = true;
+
+  let upInterval = setInterval(() => {
+    if (position >= 150) {
+      // Descendo
+      clearInterval(upInterval);
+
+      let downInterval = setInterval(() => {
+        if (position <= 0) {
+          clearInterval(downInterval);
+          isJumping = false;
+        } else {
+          position -= 20;
+          dino.style.bottom = position + 'px';
+        }
+      }, 80);
+    } else {
+      // Subindo
+      position += 20;
+      dino.style.bottom = position + 'px';
+    }
+  }, 20);
+}
+
+function createCactus() {
+  const cactus = document.createElement('div');
+  let cactusPosition = 1000;
+  let randomTime = Math.random() * 6000;
+
+  if (isGameOver) return;
+
+  cactus.classList.add('cactus');//adicionando uma class
+  background.appendChild(cactus); //adicionando a imagem do cactus
+  cactus.style.left = cactusPosition + 'px';//posição
+
+  let leftTimer = setInterval(() => {
+    if (cactusPosition < -60) {
+      // Saiu da tela
+      clearInterval(leftTimer);
+      background.removeChild(cactus);
+    } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+      // Game over
+      clearInterval(leftTimer);
+      isGameOver = true;
+      document.body.innerHTML = '<h1 class="game-over">Fim do JOGO</h1>';
+    } else {
+      cactusPosition -= 10;// velocidade do cactus
+      cactus.style.left = cactusPosition + 'px';
+    }
+  }, 20);
+
+  setTimeout(createCactus, randomTime);
+}
+
+createCactus();
+document.addEventListener('keyup', handleKeyUp);
